0432996d280d65456c01ad5d977e2f86fc8e0966
zuleika <njokizuleika@gmail.com>
2023-04-14 14:02:54 +0300

    done sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const formData ={
+    name: "Zuleika",
+    email : "njokizuleika@gmail.com",
+}
+function submitData() {
+
+ fetch("http://localhost:3000/users", {
+   method: "POST",
+   headers: {
+     "Content-Type": "application/json",
+     Accept: "application/json",
+   },
+   body: JSON.stringify(formData),
+ })
+   .then((response) => response.json())
+   .then((data) => {
+     console.log(data);
+     document.body.innerHTML += `<p>${data.id}</p>`;
+   })
+   .catch((error) => {
+       alert("Bad things!");
+     console.log(error.message);
+     document.body.innerHTML += `<p>${error.message}</p>`;
+   });
+}
